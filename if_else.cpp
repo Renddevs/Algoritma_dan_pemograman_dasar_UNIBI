ce4d4ec6c2b6b1ddd8030f39fb4c66549a43c6a0
Rendyscarlet123 <rendy9622@gmail.com>
2019-11-28 13:24:48 +0700

    use if_else statement

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main(){
+	int a;
+	a = 50
+	scanf("%i");
+	if(umur > 50){
+		printf("Besar");
+	}else if(umur > 19 && umur < 50 ){
+		printf("Sedang");
+	}else{
+		printf("Kecil");
+	}
+}
