b9201875482eb2d8d9486b31ba0dc342ad68ffa6
RendyScarlet123 <rendy9622@gmail.com>
2019-12-12 04:42:57 +0700

    update

---
+++ switchCase.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(){
+   int x;
+   while(x < 100){
+   	printf("Hello Word\n");
+   	x++;
+   }
+   for(int i= 0;i<100;i++){
+   	printf("Hello");
+   }
+   getch();
+}
