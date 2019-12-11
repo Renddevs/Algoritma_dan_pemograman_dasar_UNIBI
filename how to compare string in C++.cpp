56757ae53b342e487cc5b80b5f7ef036e90f3397
RendyScarlet123 <rendy9622@gmail.com>
2019-12-12 04:42:40 +0700

    pakai strcmp()

---
+++ how to compare string in C++.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+   char name[10];
+   scanf("%s",name);
+   if(strcmp(name,"Rendy") == 0){
+   	printf("Well done");
+   }
+   getch();
+}
